9bb5d98011ed65b16fdd22b3befc7710d9052ade
didithari <adityaqaharii@gmail.com>
2023-11-03 10:38:25 +0700

    Third commit

---
+++ src/main/java/org/example/Calculator.kt
@@ -0,0 +1,17 @@
+package org.example
+
+
+//Membuat class dengan nama Calculator
+class Calculator {
+
+
+   // Kita buat Method dua data dan return dari sum data tersebut
+     fun add(first: Int, second: Int): Int {
+        return first + second
+    }
+
+    // buat Method sub baru
+    fun sub(first: Int, second: Int): Int {
+        return first - second
+    }
+}
--- src/test/java/org/example/CalculatorTest.kt
+++ src/test/java/org/example/CalculatorTest.kt
@@ -5,14 +5,19 @@ import org.junit.jupiter.api.Assertions.*
 
 //ab
 
+
+//Membuat class dengan nama CalculatorTest
 class CalculatorTest {
 
+    //Disini saya membuat Method dan menggunakan assertEquals untuk membandingkan dua data
     @Test
     fun add() {
         val calculator = Calculator()
         val expected = 10
         assertEquals(expected, calculator.add(5, 5))
     }
+
+    //Disini  membuat Method sub untuk memperbaiki sub Method Calculator
     @Test
     fun `Test calculator can subtract two numbers`() {
         val calculator = Calculator()
