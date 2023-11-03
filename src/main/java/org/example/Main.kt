00a7809594f446ed0f9d7fc1d8792c587c72d6b8
didithari <adityaqaharii@gmail.com>
2023-11-03 10:23:34 +0700

    first commit

---
+++ src/main/java/org/example/Main.kt
@@ -0,0 +1,10 @@
+package org.example
+
+class Calculator {
+    fun add(first: Int, second: Int): Int {
+        return first + second
+    }
+    fun sub(first: Int, second: Int): Int {
+        return first - second
+    }
+}
---
+++ src/test/java/org/example/CalculatorTest.kt
@@ -0,0 +1,18 @@
+package org.example
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
+
+
+class CalculatorTest {
+
+    @Test
+    fun add() {
+        val calculator = Calculator()
+        val expected = 10
+        assertEquals(expected, calculator.add(5, 5))
+    }
+    @Test
+    fun `Test calculator can subtract two numbers`() {
+        val calculator = Calculator()
+        assertEquals(5, calculator.sub(15, 10))}}
